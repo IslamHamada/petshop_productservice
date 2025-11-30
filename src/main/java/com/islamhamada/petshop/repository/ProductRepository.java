f32529b9c9a2d1ceccbedbf77c77e03e3b86ef30
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:05:22 +0100

    add getDistinctUtilities() function/query

--- src/main/java/com/islamhamada/petshop/repository/ProductRepository.java
+++ src/main/java/com/islamhamada/petshop/repository/ProductRepository.java
@@ -2,9 +2,13 @@ package com.islamhamada.petshop.repository;
 
 import com.islamhamada.petshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("select distinct utility from Product")
+    public List<String> getDistinctUtilities();
 }
