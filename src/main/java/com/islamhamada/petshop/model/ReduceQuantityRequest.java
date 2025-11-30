682e75e028895e3e809a2fba6a042b854a69a601
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:12:13 +0100

    Add @Positive validation to the request

--- src/main/java/com/islamhamada/petshop/model/ReduceQuantityRequest.java
+++ src/main/java/com/islamhamada/petshop/model/ReduceQuantityRequest.java
@@ -1,8 +1,10 @@
 package com.islamhamada.petshop.model;
 
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class ReduceQuantityRequest {
+    @Positive
     int amount;
 }
