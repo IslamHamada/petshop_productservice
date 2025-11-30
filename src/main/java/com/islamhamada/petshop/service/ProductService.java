0347e8c3979257e88ac5dea8942bdeba07a05742
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:26:42 +0100

    remove useless imports

--- src/main/java/com/islamhamada/petshop/service/ProductService.java
+++ src/main/java/com/islamhamada/petshop/service/ProductService.java
@@ -3,9 +3,6 @@ package com.islamhamada.petshop.service;
 import com.islamhamada.petshop.contracts.dto.ProductDTO;
 import com.islamhamada.petshop.entity.Product;
 import com.islamhamada.petshop.model.ProductRequest;
-import com.islamhamada.petshop.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
