2e936e83af85322155c96f044475e0d5dc041734
IslamHamada <Islamhamada222@gmail.com>
2025-11-30 20:11:52 +0100

    Add @NotBlank validation to the request

--- src/main/java/com/islamhamada/petshop/model/ProductRequest.java
+++ src/main/java/com/islamhamada/petshop/model/ProductRequest.java
@@ -1,8 +1,10 @@
 package com.islamhamada.petshop.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ProductRequest {
+    @NotBlank
     private String name;
 }
