b08336c3416b0e3360b06a03c3814e7424f2aef1
IslamHamada <Islamhamada222@gmail.com>
2025-12-05 17:58:41 +0100

    add @AllArgsConstructor to ProductRequest (for testing)

--- src/main/java/com/islamhamada/petshop/model/ProductRequest.java
+++ src/main/java/com/islamhamada/petshop/model/ProductRequest.java
@@ -1,9 +1,11 @@
 package com.islamhamada.petshop.model;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ProductRequest {
     @NotBlank
     private String name;
