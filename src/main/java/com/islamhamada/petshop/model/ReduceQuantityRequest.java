dcac7aaf5e0d5467bf6ce63f7fdb7379625adb0d
IslamHamada <islamhamada222@gmail.com>
2025-12-31 11:19:39 +0100

    add @AllArgsConstructor to ReduceQuntityRequest

--- src/main/java/com/islamhamada/petshop/model/ReduceQuantityRequest.java
+++ src/main/java/com/islamhamada/petshop/model/ReduceQuantityRequest.java
@@ -1,9 +1,11 @@
 package com.islamhamada.petshop.model;
 
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ReduceQuantityRequest {
     @Positive
     int amount;
